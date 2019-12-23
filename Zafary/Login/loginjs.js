ccf3e2d985c945008873f3ba221fd5aa5ab59780
Ivan-Alvarez-bash <58012571+Ivan-Alvarez-bash@users.noreply.github.com>
2019-12-23 01:29:40 -0300

    Vamos

--- Zafary/Login/loginjs.js
+++ Zafary/Login/loginjs.js
@@ -1,1 +1,23 @@
+const boton = window.document.querySelector('#boton');
+boton.addEventListener('click', validar);
+
+function validar() {
+ 
+    const email = window.document.querySelector("#email");
+    const ingresoMail = email.value;
+
+    const pass = window.document.querySelector("#pass");
+    const ingresoPass = pass.value;
+    
+    console.log(ingresoMail);
+    console.log(ingresoPass);
+
+    if (ingresoMail === 'ejemplo@gmail.com' && ingresoPass === '123456') {
+        console.log('Iniciando sesión...');
+    } 
+    
+    else {
+        console.log('E-mail y/o contraseña incorrectos');
+    }
+}
 
